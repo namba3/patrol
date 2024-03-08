54e7d36c51d71c763e3bc94be427a37ebd4d19c7
namba3 <namba3@users.noreply.github.com>
2024-03-08 21:18:36 +0900

    update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::future::IntoFuture;
+use std::sync::mpsc;
+
 use clap::Parser;
 use env_logger::Env;
 use log::{error, info};
@@ -6,6 +9,8 @@ use patrol::application::{App, SelectivePoller};
 use patrol::infrastructure::{
     HttpPoller, TomlConfigRepository, TomlDataRepository, WebDriverPoller,
 };
+use tokio::io::AsyncBufReadExt;
+use tokio::sync::oneshot;
 
 #[derive(Parser)]
 #[clap(author, version, about)]
@@ -72,8 +77,33 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         interval_limit,
     );
 
-    if let Err(why) = app.run().await {
-        error!("{why}")
+    let (tx, rx) = oneshot::channel();
+    tokio::spawn(async {
+        let stdin = tokio::io::BufReader::new(tokio::io::stdin());
+        let mut lines = stdin.lines();
+
+        loop {
+            let line = lines.next_line().await;
+            match line.as_ref().map(|x| x.as_ref().map(|y| y.as_str())) {
+                Ok(Some("q")) => break,
+                Ok(_) => (),
+                Err(_why) => {
+                    //error!("{why}")
+                    break;
+                }
+            }
+        }
+
+        tx.send(())
+    });
+
+    tokio::select! {
+        _quit = rx  => (),
+        result = app.run() => {
+            if let Err(why) = result {
+                error!("{why}")
+            }
+        },
     }
 
     Ok(())
